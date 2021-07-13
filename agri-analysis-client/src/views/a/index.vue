c5525c1bebee470519163145e8886cedab97dc80
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-07-13 17:03:03 +0800

    demo

--- agri-analysis-client/src/views/a/index.vue
+++ agri-analysis-client/src/views/a/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>A</div>
+  <div>
+    <div>A</div>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
