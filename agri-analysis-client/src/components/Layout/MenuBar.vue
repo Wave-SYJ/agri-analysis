4100bda9647971fe5ac19a80f451a29941392272
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-07-14 15:45:42 +0800

    fix bugs

--- agri-analysis-client/src/components/Layout/MenuBar.vue
+++ agri-analysis-client/src/components/Layout/MenuBar.vue
@@ -7,8 +7,8 @@
         <template slot="title"><i class="el-icon-view"></i><router-link to="/dashboard/a" class="a">数据监控</router-link></template>
       </el-menu-item>
       <el-submenu index="2">
-        <template slot="title"><i class="el-icon-search"></i><router-link to="/dashboard/b" class="a">数据查询</router-link></template>
-        <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-location-outline"></i>区域行情</el-menu-item>
+        <template slot="title"><i class="el-icon-search"></i>数据查询</template>
+        <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-location-outline"></i><router-link to="/dashboard/b">区域行情</router-link></el-menu-item>
         <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-ship"></i>品种对比</el-menu-item>
         <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-cherry"></i>农产品售卖行情</el-menu-item>
       </el-submenu>
