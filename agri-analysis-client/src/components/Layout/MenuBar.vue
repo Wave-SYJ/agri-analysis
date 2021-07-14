d0f9b0217288acd968de48ec8bcbfe90240c4151
Wave-SYJ <sun1yu1jia1@outlook.com>
2021-07-14 15:28:13 +0800

    fix bug

--- agri-analysis-client/src/components/Layout/MenuBar.vue
+++ agri-analysis-client/src/components/Layout/MenuBar.vue
@@ -4,10 +4,10 @@
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
     <el-menu :default-openeds="['1', '4']">
       <el-menu-item index="1">
-        <template slot="title"><i class="el-icon-view"></i><router-link to="/a" class="a">数据监控</router-link></template>
+        <template slot="title"><i class="el-icon-view"></i><router-link to="/dashboard/a" class="a">数据监控</router-link></template>
       </el-menu-item>
       <el-submenu index="2">
-        <template slot="title"><i class="el-icon-search"></i><router-link to="/b" class="a">数据查询</router-link></template>
+        <template slot="title"><i class="el-icon-search"></i><router-link to="/dashboard/b" class="a">数据查询</router-link></template>
         <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-location-outline"></i>区域行情</el-menu-item>
         <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-ship"></i>品种对比</el-menu-item>
         <el-menu-item index="2-1" style="color=rgb(0,0,0)"><i class="el-icon-cherry"></i>农产品售卖行情</el-menu-item>
