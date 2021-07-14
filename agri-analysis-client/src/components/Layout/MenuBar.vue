ed34196ff8bd7536dc0ec3efc7c30d8c6e1b860a
zzp <1755184552@xn--ldr802c>
2021-07-14 14:43:28 +0800

    test

--- agri-analysis-client/src/components/Layout/MenuBar.vue
+++ agri-analysis-client/src/components/Layout/MenuBar.vue
@@ -1,19 +1,45 @@
 <template>
-  <ul class="menu-bar">
-    <li><router-link to="/a">Go to A</router-link></li>
-    <li><router-link to="/b">Go to B</router-link></li>
-  </ul>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-menu-item index="1">
+      <i class="el-icon-menu"></i>
+      <span slot="title">数据更新</span>
+    </el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>数据查询</span>
+      </template>
+      <el-menu-item index="2-1">区域行情查询</el-menu-item>
+      <el-menu-item index="2-2">品种对比</el-menu-item>
+      <el-menu-item index="2-3">售卖行情</el-menu-item>
+    </el-submenu>
+
+    <el-menu-item index="3">
+      <i class="el-icon-document"></i>
+      <span slot="title">数据预测</span>
+    </el-menu-item>
+
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">数据修改</span>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
-
-<style scoped>
-.menu-bar {
-  padding-top: 10px;
-  height: 200px;
-  width: 100px;
-  background-color: orange;
-}
-</style>
