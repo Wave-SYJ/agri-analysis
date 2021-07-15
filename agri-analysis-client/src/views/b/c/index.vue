a11728e40ca581e623e968db1e5e252d58db169d
zzp <1755184552@xn--ldr802c>
2021-07-15 17:43:08 +0800

    test

--- agri-analysis-client/src/views/b/c/index.vue
+++ agri-analysis-client/src/views/b/c/index.vue
@@ -1,39 +1,160 @@
 <template>
   <div>
-    <div>B</div>
-    <div>
-      <el-input placeholder="请输入内容" v-model="input1">
-        <template slot="prepend">Http://</template>
-      </el-input>
-    </div>
-    <div style="margin-top: 15px;">
-      <el-input placeholder="请输入内容" v-model="input2">
-        <template slot="append">.com</template>
-      </el-input>
-    </div>
-    <div style="margin-top: 15px;">
-      <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-        <el-select v-model="select" slot="prepend" placeholder="请选择">
-          <el-option label="餐厅名" value="1"></el-option>
-          <el-option label="订单号" value="2"></el-option>
-          <el-option label="用户电话" value="3"></el-option>
-        </el-select>
-        <el-button slot="append" icon="el-icon-search"></el-button>
-      </el-input>
-    </div>
+    <el-row style="margin-top:30px">
+      <span style="color: #409eff">请选择省</span>
+      <span style="margin-left: 130px; color: #409eff">请选择市</span>
+      <span style="margin-left: 130px; color: #409eff">请选择市场</span>
+      <span style="margin-left: 130px; color: #409eff">请选择农产品种类</span>
+    </el-row>
+    <el-row>
+      <el-select v-model="value1" change="province_select" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+
+      <el-select
+        v-model="value2"
+        change="city_select"
+        collapse-tags
+        style="margin-left: 64px"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-select
+        v-model="value3"
+        change="market_select"
+        collapse-tags
+        style="margin-left: 64px"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-select
+        v-model="value4"
+        collapse-tags
+        style="margin-left: 80px"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-button
+        round
+        style="margin-left: 64px; color: white; background-color: #67c23a"
+        @click="queryclick"
+        >查询售卖行情</el-button
+      >
+    </el-row>
+    <el-row class="a">
+      <span style="color: ##67c23a">您查询的农作物数据天数为:</span
+      ><span style="color: #409eff">{{ x }}</span>
+    </el-row>
+    <el-row class="b"
+      ><span style="color: #409eff">请选择行情显示时间间隔</span>
+      <el-select
+        style="margin-left: 20px"
+        v-model="timespan"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in time_span"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-button
+        round
+        style="margin-left: 64px; color: white; background-color: #409eff"
+        @click="displaychart"
+        >显示图表</el-button
+      >
+    </el-row>
+    <img style="margin-top: 10px" src="paris.jpg" alt="绘制" />
   </div>
 </template>
-
-<script>//区域行情
+ 
+<script>
 export default {
   data() {
     return {
-      input1: '',
-      input2: '',
-      input3: '',
-      select: ''
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      time_span: [
+        {
+          value: "年",
+          label: "年",
+        },
+        {
+          value: "月",
+          label: "月",
+        },
+        {
+          value: "日",
+          label: "日",
+        },
+      ],
+      value1: null,
+      value2: null,
+      value3: null,
+      value4: null,
+      timespan: null,
+      x: null,
     };
-  }
+  },
+  methods: {
+    queryclick() {
+      this.x = console.log(this.value1);
+    },
+    displaychart() {},
+    province_select() {},
+    city_select() {},
+    market_select() {},
+  },
 };
 </script>
 
@@ -43,5 +164,19 @@ export default {
 }
 .input-with-select .el-input-group__prepend {
   background-color: #fff;
+}
+.text {
+  -moz-column-count: 4; /* Firefox */
+  column-count: 4;
+}
+.a {
+  width: 100%;
+  height: 50px;
+  margin-top: 50px;
+}
+.b {
+  width: 100%;
+  height: 100px;
+  margin-top: 25px;
 }
 </style>
