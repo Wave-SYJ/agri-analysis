5f4fe862f9ad135919015dc306fe34122d7c4a4f
zzp <1755184552@xn--ldr802c>
2021-07-16 11:12:21 +0800

    7.16

--- agri-analysis-client/src/main.js
+++ agri-analysis-client/src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/style/global.scss'
 import Fragment from 'vue-fragment'
+import echarts from 'echarts'
 
+
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 Vue.use(ElementUI);
